d8d146d66dd5948722b1b4dc513322650adf9521
Sylvia Chen <kelly901021@gmaiol.com>
2023-08-03 19:27:58 +0800

    編輯頁面

--- resources/views/fractions/index.blade.php
+++ resources/views/fractions/index.blade.php
@@ -4,8 +4,8 @@
 <header>
     <div class="container pt-4 text-center">
         <div class="jumbotron bg-info text-white">
-            <h1 class="display-4" style="font-size: 8vmin;">SOKA好聲音</h1></h1>
-            <p class="lead" style="font-size: 4vmin;">悠揚樂聲，伴你一生</p>
+            <h1 class="display-4" style="font-size: 8vmin;">SOKA積分版</h1>
+            <p class="lead" style="font-size: 4vmin;">分數是什麼呢?</p>
         </div>
     </div>
 </header>
@@ -20,19 +20,19 @@
         <tbody>
             <tr>
                 <td>超級瑪利歐</td>
-                <td></td>
+                <td id="fraction-one">fraction</td>
             </tr>
             <tr>
                 <td>咒術迴戰 領域展開</td>
-                <td></td>
+                <td id="fraction-two">fraction</td>
             </tr>
             <tr>
                 <td>無所畏懼小小兵</td>
-                <td></td>
+                <td id="fraction-three">fraction</td>
             </tr>
             <tr>
                 <td>名偵探柯南</td>
-                <td></td>
+                <td id="fraction-four">fraction</td>
             </tr>
         </tbody>
     </table>
